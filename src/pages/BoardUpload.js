df409bce772996dc9b89c4eaab44d5acfba5adfc
kellylee23 <dmstj4632@hufs.ac.kr>
2025-05-10 00:34:20 +0900

    initial settings

---
+++ src/pages/BoardUpload.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BoardUpload() {
+  return <div></div>;
+}
+
+export default BoardUpload;
---
+++ src/pages/ManageDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ManageDetail() {
+  return <div></div>;
+}
+
+export default ManageDetail;
